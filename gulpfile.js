ae78aa4e330fa95b99dec1ec5c4626dd4ff25a3f
Uladzimir Shein <uladzimir_shein@epam.com>
2015-01-13 23:02:22 +0300

    Added livereload 0.0.19

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
+var livereload = require('gulp-livereload');
 
 var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
 var LessPluginAutoPrefix = require('less-plugin-autoprefix');
 var autoprefix = new LessPluginAutoPrefix({browsers: ['last 2 versions']});
-gulp.task('usemin', ['ng-templates', 'app-version', 'compile-less'], function () {
+gulp.task('usemin', ['ng-templates&app-version', 'compile-less'], function () {
     var usemin = require('gulp-usemin');
     var uglify = require('gulp-uglify');
     var minifyHtml = require('gulp-minify-html');
@@ -13,22 +14,15 @@ gulp.task('usemin', ['ng-templates', 'app-version', 'compile-less'], function ()
     var ngAnnotate = require('gulp-ng-annotate');
     var inject = require('gulp-inject');
     var processhtml = require('gulp-processhtml');
-
-    var pkg = require('./package.json');
-
     var cssBase64 = require('gulp-css-base64');
     return gulp.src('index.html')
-        .pipe(inject(gulp.src('.tmp/templates.js', {read: false}),
-            {starttag: '<!-- inject:.tmp/templates:js -->'}
-        ))
-        .pipe(inject(gulp.src('.tmp/package.js', {read: false}),
-            {starttag: '<!-- inject:.tmp/package:js -->'}
+        .pipe(inject(gulp.src(['.tmp/templates.js', '.tmp/package.js'], {read: false}),
+            {starttag: '<!-- inject:.tmp/templates-and-app-version:js -->'}
         ))
         .pipe(usemin({
             html: [processhtml({
                 commentMarker: 'process',
-                process: true,
-                data: {version: pkg.version}
+                process: true
             }), minifyHtml()],
             css: [cssBase64({maxWeightResource: 10000000}), minifyCss(), 'concat', rev()],
             js: [sourcemaps.init(), ngAnnotate(), uglify(), rev(), sourcemaps.write('.')]
@@ -36,21 +30,20 @@ gulp.task('usemin', ['ng-templates', 'app-version', 'compile-less'], function ()
         .pipe(gulp.dest('build/'));
 });
 
-gulp.task('ng-templates', ['clean'], function () {
-    var templateCache = require('gulp-angular-templatecache');
-    return gulp.src('src/**/*.html')
-        .pipe(templateCache({
-            root: 'src/',
-            module: 'sensors-app'
-        }))
-        .pipe(gulp.dest('.tmp'));
-});
-
-gulp.task('app-version', function () {
+gulp.task('ng-templates&app-version', ['clean'], function () {
     var gulpNgConfig = require('gulp-ng-config');
     gulp.src('package.json')
         .pipe(gulpNgConfig('package-json'))
         .pipe(gulp.dest('.tmp/'));
+
+    var templateCache = require('gulp-angular-templatecache');
+    var pkg = require('./package.json');
+    return gulp.src('src/**/*.html')
+        .pipe(templateCache({
+            root: 'src/',
+            module: pkg.ngAppName
+        }))
+        .pipe(gulp.dest('.tmp'));
 });
 
 var clean = require('gulp-clean');
@@ -63,11 +56,7 @@ gulp.task('remove .tmp', ['usemin'], function () {
 });
 
 gulp.task('less', ['clean-less'], function () {
-    return gulp.src('src/**/*.less')
-        //.pipe(sourcemaps.init())
-        .pipe(less({plugins: [autoprefix]}))
-        //.pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('src'));
+    return compileLess('src/**/*.less', 'src');
 });
 
 gulp.task('clean-less', function () {
@@ -79,20 +68,28 @@ gulp.task('clean-less', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch('src/**/*.less', function (file) {
-        var path = file.path;
-        if (path.indexOf('less') === -1 || file.type !== 'changed') {
-            return;
-        }
-        console.log('changed: ' + path); // jshint ignore:line
-        var folder = path.slice(0, path.lastIndexOf('\\'));
-        gulp.src(path)
-            //.pipe(sourcemaps.init())
-            .pipe(less({plugins: [autoprefix]}))
-            //.pipe(sourcemaps.write('.'))
-            .pipe(gulp.dest(folder));
+    livereload.listen();
+    gulp.watch(['src/**/*.js', 'src/**/*.html', 'index.html'], function (file) {
+        console.log('script changed: ' +  file.path); // jshint ignore:line
+        gulp.src(file.path).pipe(livereload());
     });
-});
+    gulp.watch('src/**/*.less', function (file) {
+        if (file.type === 'changed') {
+            var path = file.path;
+            console.log('changed: ' + path); // jshint ignore:line
+            var folder = path.slice(0, path.lastIndexOf('\\'));
+            compileLess(path, folder);
+        }
+    });
+}).on('change', livereload.changed);
+
+function compileLess(src, dest) {
+    return gulp.src(src)
+        //.pipe(sourcemaps.init())
+        .pipe(less({plugins: [autoprefix]}))
+        //.pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest(dest));
+}
 
 gulp.task('jshint', function () {
     var jshint = require('gulp-jshint');
@@ -102,7 +99,7 @@ gulp.task('jshint', function () {
 });
 
 gulp.task('compile-less', ['clean-less', 'less']);
-gulp.task('build', ['clean', 'compile-less', 'ng-templates', 'app-version', 'usemin', 'remove .tmp']);
+gulp.task('build', ['clean', 'compile-less', 'ng-templates&app-version',  'usemin', 'remove .tmp']);
 
 gulp.task('connect', function () {
     return require('gulp-connect').server({
