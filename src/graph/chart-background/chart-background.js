799b8ff7457a821bfc3d7ae4567d3d21248068e4
Uladzimir Shein <uladzimir_shein@epam.com>
2015-01-17 17:41:24 +0300

    0.0.23 Added monthly usage page and monthly usage menu.

--- src/graph/chart-background/chart-background.js
+++
@@ -1,9 +0,0 @@
-angular.module('graph.chart-background', [
-    'graph.area-chart'
-]).directive('chartBackground', function () {
-    return {
-        transclude: true,
-        scope: {values: '=?', axisOutline: '=?'},
-        templateUrl: 'src/graph/chart-background/chart-background.html'
-    };
-});
